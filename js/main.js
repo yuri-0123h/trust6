aef58352ef9fe73574b45e974113df574a39c18b
yuri-0123h <137026296+yuri-0123h@users.noreply.github.com>
2023-12-17 03:14:26 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $(function () {
   $(".toggle-btn").on("click", function () {
     $(".header-sp").toggleClass("open");
   });
-  // $(".arrow-sp").on("click", function () {
-  //   $(".header-sp-menu").toggleClass("open-menu");
-  // });
-  $(".arrow-sp").on("click", function () {
-    $(".accordion-category-box").toggleClass("down");
-  });
+
 
 2
 3
 $('.accordion-category').click(function(){
     $(this).next().slideToggle(300);
+    $(this).toggleClass("open", 300);
+});
+$('.category-item').click(function(){
+  $(this).next('.dropdown-list2').slideToggle(300);
+  $(this).toggleClass("open", 300);
 });
 
 
